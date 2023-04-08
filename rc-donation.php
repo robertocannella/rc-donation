b5dd45eacf9191f2261e776d48cb3e4b105614bc
robertocannella <robertocannella@gmail.com>
2023-04-08 19:37:16 -0400

    Add index.js, Add rc_donation_init() to rc_docation.php

--- rc-donation.php
+++ rc-donation.php
@@ -9,3 +9,12 @@ Author: robertocannella
 Author URI: https://www.robertocannella.com
 License: A "Slug" license name e.g. GPL2
 */
+
+
+// Load block.json
+function rc_donation_init(){
+        // This grabs block.json from the current directory
+        register_block_type_from_metadata( __DIR__ );
+}
+
+add_action('init',"rc_donation_init");
