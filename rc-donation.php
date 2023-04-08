dd3a9ecc8f7ab69d80dfe2c397d962ace870595a
robertocannella <robertocannella@gmail.com>
2023-04-08 19:22:35 -0400

    Initial commit

---
+++ rc-donation.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: RC Donation
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A Simple Donation Popup Tool.
+Version: 1.0
+Author: robertocannella
+Author URI: https://www.robertocannella.com
+License: A "Slug" license name e.g. GPL2
+*/
